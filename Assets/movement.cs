b53c7929f39e01ad16c8fd3bf34b6062e5b22049
Avers03 <averroes0309@student.ub.ac.id>
2022-03-06 14:39:10 +0700

    batas map

--- Assets/movement.cs
+++ Assets/movement.cs
@@ -7,6 +7,8 @@ public class movement : MonoBehaviour
     Rigidbody2D Rb;
     public Vector2 targetPosition;
     public float speed;
+    bool moveDone;
+
 
     // Start is called before the first frame update
     void Start()
@@ -17,11 +19,19 @@ public class movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Mouse0))
-           {
+        if(Input.GetKeyDown(KeyCode.Mouse0) && moveDone == false)
+        {
             targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            moveDone = true;
         }
  
         transform.position = Vector2.MoveTowards(transform.position, targetPosition, Time.deltaTime * speed);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag=="BatasMap")
+        {
+            speed = 0;
+        }
     }
 }
