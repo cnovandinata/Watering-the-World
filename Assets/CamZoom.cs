8145693edde52ae1cb80c1e71191c63e8713f954
cnovandinata <95232744+cnovandinata@users.noreply.github.com>
2022-03-13 21:02:55 +0700

    Tambahan sapi monyet + gerak acak, perubahan border map

--- Assets/CamZoom.cs
+++ Assets/CamZoom.cs
@@ -16,10 +16,19 @@ public class CamZoom : MonoBehaviour
     void LateUpdate()
     {
         if (Input.mouseScrollDelta.y > 0)
-            if (gameCamera.orthographicSize - Input.mouseScrollDelta.y > 0.25f)
+        {
+            if (gameCamera.orthographicSize - Input.mouseScrollDelta.y > 0.5f)
                 gameCamera.orthographicSize = gameCamera.orthographicSize - Input.mouseScrollDelta.y;
+            else
+                gameCamera.orthographicSize = 0.5f;
+        }
         if (Input.mouseScrollDelta.y < 0)
+        {
             if (gameCamera.orthographicSize - Input.mouseScrollDelta.y < 5)
                 gameCamera.orthographicSize = gameCamera.orthographicSize - Input.mouseScrollDelta.y;
+            else
+                gameCamera.orthographicSize = 5f;
+        }
+
     }
 }
---
+++ Assets/randomMovement.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randomMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public Vector2 targetPosition;
+    float sp = 0.25f;
+    float timer, movementTime;
+    public Animator animator;
+    bool flipped = false;
+    Vector3 newScale;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        targetPosition = new Vector2(transform.position.x, transform.position.y);
+        movementTime = Random.Range(10, 20);
+        timer = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        timer += Time.deltaTime;
+        if(timer >= movementTime)
+        {
+            targetPosition = new Vector2(transform.position.x + Random.Range(-0.5f, 0.5f), transform.position.y + Random.Range(-0.5f, 0.5f));
+            timer = 0f;
+            movementTime = Random.Range(15, 25);
+            sp = 0.25f;
+        }
+        transform.position = Vector2.MoveTowards(transform.position, targetPosition, Time.deltaTime * sp);
+        if (transform.position.x == targetPosition.x && transform.position.y == targetPosition.y)
+            animator.SetBool("Move", false);
+        else
+            animator.SetBool("Move", true);
+        if (flipped)
+        {
+            if (targetPosition.x > transform.position.x)
+            {
+                newScale = transform.localScale;
+                newScale.x *= -1;
+                transform.localScale = newScale;
+                flipped = false;
+            }
+        }
+        else
+        {
+            if (targetPosition.x < transform.position.x)
+            {
+                newScale = transform.localScale;
+                newScale.x *= -1;
+                transform.localScale = newScale;
+                flipped = true;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "BatasMap")
+        {
+            sp = 0;
+        }
+    }
+}
